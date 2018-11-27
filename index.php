91109c7b15bb7464916e88dcc38e18cac92e5a7c
thawdahtun <reporterlayhlaing@gmail.com>
2018-11-27 23:39:38 +0630

    Step 6

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post(); ?>
+
+<h2><?php the_title() ?></h2>
+<?php the_content() ?>
+
+<?php endwhile;
+
+else :
+	echo '<p>There are no posts!</p>';
+
+endif;
+
+?>
